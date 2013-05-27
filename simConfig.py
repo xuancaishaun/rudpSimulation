ebd9a467daad63d95f027ea897bcc3f140731ac8
“kalasoo” <ym.kalasoo@gmail.com>
2013-05-27 15:00:24 +0000

    edit for demo

--- simConfig.py
+++ simConfig.py
@@ -1,24 +1,18 @@
 hostnames = [
-	'planetlab4.williams.edu',
-	'planetlab2.fri.uni-lj.si', 
-	'planetlab2-santiago.lan.redclara.net', 
-	'plab3.eece.ksu.edu', 
-	'planetlab-3.cmcl.cs.cmu.edu', 
-	'planet-lab2.itba.edu.ar', 
-	'pl02.comp.polyu.edu.hk'
+	"ricepl-1.cs.rice.edu",
+	"planet3.cs.ucsb.edu",
+	"planetlab-01.vt.nodes.planet-lab.org",
+	"planetlab2-santiago.lan.redclara.net"
 ]
 hosts = [
-	'137.165.1.114', 
-	'212.235.189.115', 
-	'200.0.206.203', 
-	'129.130.252.140', 
-	'128.2.211.115', 
-	'190.227.163.141', 
-	'202.125.215.12'
+	"128.42.142.41",
+	"128.111.52.63",
+	"198.82.160.238",
+	"200.0.206.203"
 ]
 
 #We use the first two hosts
-hosts = hosts[:2]
+hosts = hosts[:4]
 
 # Specific port for both sending and receiving data
 PEER_PORT = 39951
@@ -28,7 +22,7 @@ PEER_PORT = 39951
 VAR_SEND_INT = 100	# gamma; exponential distri.
 
 # Prob. of sending reliable messages
-VAR_SEND_REL = 0.7	# uniform distri.
+VAR_SEND_REL = 0.2	# uniform distri.
 
 # Message size follows normal distri.
 # E.g. int(abs(gauss( average, std. )))
@@ -36,7 +30,7 @@ VAR_MSG_AVG = 1024
 VAR_MSG_STD = 100
 
 # Flag for output printing 
-OUTPUT_CLEAN = True 
+OUTPUT_CLEAN = False 
 
 # Local testing
 LOCAL_TEST = False
